3b3798e1378392be5ecd5e75101972187b2f2fff
MakoskiVictor <makoski.ed@gmail.com>
2024-03-30 13:06:19 -0300

    Primitive types

--- Program.cs
+++ Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+
+internal class Program
+{
+  private static void Main()
+  {
+    // Console.WriteLine("Hello, World!");
+    // Console.WriteLine("The second line");
+    // Console.Write("This is the first Write");
+    // Console.Write("This is the second Write");
+
+    PrintIn(byteNumber);
+    PrintIn(shortNumber);
+    PrintIn(intNumber);
+    //PrintIn(longNumber);
+    PrintInDouble(floatNumber);
+    PrintInDouble(doubleNumber);
+   // PrintIn(decimalNumber);
+    Console.WriteLine($"This is the constant: {PI}");
+  }
+  // Primitive Types
+  static readonly byte byteNumber = 255; // From 0 to 255
+  static readonly short shortNumber = 32767; // From -32768 to 32767
+  static readonly int intNumber = 2100000000; // From -2.1B to 2.1B
+  //static readonly long longNumber = 2200000000; // Infinite
+  //REAL NUMBERS
+  static readonly float floatNumber = 2.14f; // Need use "f" at the end
+  static readonly double doubleNumber = 2.14; // ==> This is the default
+  //static decimal decimalNumber = 2.14m; // Need use "m" at the end
+
+  // CHARACTER
+  //char charLetter = 'd';
+
+  // BOOBLEAN
+  //bool booleanVariable = true;
+
+  // CONSTANTS
+
+  const double PI = 3.14;
+
+
+  public static void PrintIn (int arg) {
+    Console.WriteLine(arg);
+  }
+
+  public static void PrintInDouble (double arg) {
+    Console.WriteLine(arg);
+  }
+}
